1387c41e3f6267330c1c9456e805c0d27bc4ad8d
Chaoran Li <n10298827@qut.edu.au>
2019-05-22 22:47:28 +1000

    Change

--- CheckingBoardingPass/Program.cs
+++ CheckingBoardingPass/Program.cs
@@ -4,21 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CheckingBoardingPass
+namespace GenerateBoardingPass
 {
-    class Program
+    class Passenger
     {
         private string passengerName;
         private int sercurityNumber;
-        private int deptureTime;
         private const int DEPTURE_GATE = 5;
         private int seatNumber;
+        int i = 1;
 
         private int[] seats = new int[40];
         private string[] names = new string[40];
 
         static void Main(string[] args)
         {
+            int i = 1;
+            while(true) {
+
+                Console.WriteLine("Please enter Name or PRESS q to quit");
+                string input = Console.ReadLine();
+                if (input == "q" || input == "Q") {
+                    break;
+                }
+                
+                Passenger pg = new Passenger();
+                if (input.Length <= 5) {
+                    pg.passengerName = input;
+                    pg.names[i] = input;
+                } else {
+                    pg.passengerName = input.Trim();// to do
+                }
+
+                if (i <= 39) {
+                    pg.seatNumber = i;
+                    
+                } else {
+                    Console.WriteLine("No more seats can be signed...");
+                    break;
+                }
+                
+
+                Random random = new Random();
+                pg.sercurityNumber = random.Next(30000, 999999);
+
+                DateTime now = DateTime.Now;
+
+                //generate boarding pass
+                Console.WriteLine();
+                Console.WriteLine("****************************************");
+                Console.WriteLine("Passenger name: " + pg.passengerName);
+                Console.WriteLine("Seat No. " + i);
+                Console.WriteLine("Gate No. " + DEPTURE_GATE);
+                Console.WriteLine("Depature Time: " + now);
+                Console.WriteLine("****************************************");
+
+
+
+                i++;
+
+            }
+
+            Console.ReadKey();
+
         }
 
         //This method lists all passengers' information
@@ -30,10 +78,9 @@ namespace CheckingBoardingPass
         //This method displays available seats
         public void Display()
         {
-
+            Console.WriteLine("Available seats is after {0}", i);
         }
 
-
-
+        
     }
 }
