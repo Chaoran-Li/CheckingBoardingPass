02c8ded3cfd9b4fdde72050d86f11fc39221cd19
Chaoran Li <n10298827@qut.edu.au>
2019-05-22 12:28:29 +1000

    Add project files.

---
+++ CheckingBoardingPass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckingBoardingPass
+{
+    class Program
+    {
+        private string passengerName;
+        private int sercurityNumber;
+        private int deptureTime;
+        private const int DEPTURE_GATE = 5;
+        private int seatNumber;
+
+        static void Main(string[] args)
+        {
+        }
+
+        public Program(int number)
+        {
+            int[] seat = new int[number];
+            string[] name = new string[number];
+        }
+
+        //This method lists all passengers' information
+        public void Info()
+        {
+
+        }
+
+        //This method displays available seats
+        public void Display()
+        {
+
+        }
+
+
+
+    }
+}
